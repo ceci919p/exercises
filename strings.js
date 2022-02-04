774c33bbfc71d3a7802d370630ce396f660a798f
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-04 16:24:23 +0100

    exercises w strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+"use strict";
+const longline =
+  "A very, very, very, very, VERY! long line of text,\
+  that spans across the width of the editor!";
+
+const name = `Peter`;
+const drink = `Coca-cola`;
+
+const text = `Hello ${name},
+ would you like a ${drink}?`;
+
+console.log(text);
---
+++ template.js
@@ -0,0 +1,8 @@
+const name = `Cecilie`;
+const animalType = `dog`;
+const animalName = `Indy`;
+
+const text = `My name is ${name},
+I have a ${animalType} called ${animalName}`;
+
+console.log(text);
