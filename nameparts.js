fd09479a272848d00afde4bca4666b1461ac8e6e
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-14 14:51:06 +0100

    name parts function done

--- nameparts.js
+++ nameparts.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/* 
 //const name = "Peter Heronimous Lind";
 
 //const name = "Cecilie Jasmin Jørgensen";
@@ -22,4 +22,32 @@ console.log("middleName", middleName);
 
 const lastName = name.substring(name.lastIndexOf(" ") + 1);
 
-console.log("lastName", lastName);
+console.log("lastName", lastName); */
+
+//---------NAME PARTS FUNCTION EXERCISE
+
+//let fullName = "Cecilie Jasmin Jørgensen";
+
+function getNameParts(fullName) {
+  //find firstName
+  const firstName = fullName.substring(
+    fullName.indexOf(0),
+    fullName.indexOf(" ")
+  );
+
+  //find middleName
+
+  const middleName = fullName.substring(
+    fullName.indexOf(" ") + 1,
+    fullName.lastIndexOf(" ")
+  );
+
+  //find lastName
+  const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+
+  console.log(
+    `Hello my firstname is ${firstName} and my middlename is ${middleName} and last my lastname is ${lastName}`
+  );
+}
+
+getNameParts("Cecilie Jasmin Jørgensen");
